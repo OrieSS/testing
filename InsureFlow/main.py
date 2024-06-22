0f829468cc5e9e106bbd7ababd986a137224aa3e
rawat-nitin <rsnitin.inv@gmail.com>
2024-06-22 17:00:21 +0900

    This is working in container. Changed some code lines for making it platform independent.

--- InsureFlow/main.py
+++ InsureFlow/main.py
@@ -1,5 +1,6 @@
 import ssl
 import urllib.request
+import os
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -17,8 +18,15 @@ def main():
     # Train a model
     lr = create_model('lr')
 
+    # Determine the base directory path
+    base_dir = os.path.dirname(os.path.abspath(__file__))  # Gets the directory of this script
+
+    # Define the relative path within the project folder
+    model_dir = os.path.join(base_dir, 'InsureFlow', 'models')
+    os.makedirs(model_dir, exist_ok=True)
+
     # Save pipeline/model
-    save_model(lr, model_name='C:/Git/AIProject/testing/InsureFlow/deployment_12062024')
+    save_model(lr, model_name=os.path.join(model_dir, 'deployment_12062024'))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
